94fccc9a8048a6debbdcb4e0a67720a22f2330a5
ARTHUR <T@skylark.com>
2018-06-07 09:13:56 +0300

    u see

---
+++ tcp_client.py
@@ -0,0 +1,17 @@
+from socket import *
+
+
+server_address = ('127.0.0.1', 8000)
+client_socket = socket(AF_INET, SOCK_STREAM)
+try:
+        client_socket.connect(server_address)
+
+        message = input("Enter the message to send or 'q' to quit: ")
+        client_socket.send(message.encode('utf-8'))
+        response = client_socket.recv(4096)
+        print(response.decode('utf-8'))
+
+        client_socket.close()
+
+except Exception as ex:
+    print(ex)
